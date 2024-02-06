dbd63d04e30235da780090fa732f354e1172ee83
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-02-06 23:54:25 +0530

    API call in componentDidMount

--- Code/src/components/UserClass.js
+++ Code/src/components/UserClass.js
@@ -7,37 +7,49 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            count : 0,
-            count2: 1,
-            count3: 2,
+            // count : 0,
+            // count2: 1,
+            // count3: 2,
+            userInfo : "Dummy",
+
         }
-        console.log(this.props.name + "Child constructor")
+        // console.log(this.props.name + "Child constructor")
 
     }
 
-    componentDidMount(){
-        console.log(this.props.name + "child ComponentDidMount")
+    async componentDidMount(){
+        // console.log(this.props.name + "child ComponentDidMount")
+        const data = await fetch ("https://api.github.com/users/sagarika2426");
+        const json = await data.json();
+        console.log(json);
+        
+        this.setState({
+            userInfo:json,
+        })
+
     }
 
     render(){
         // Using destructuring
-        const{count, count2, count3} = this.state;
-        console.log( this.props.name + "child render");
-
+        // const{count, count2, count3} = this.state;
+        // console.log( this.props.name + "child render");
+        const {name, avatar_url} = this.state.userInfo
         return(
             <div>
-                <h1>Name: {this.props.name}</h1>
-                <h2>Count: {count}</h2>
-                <h2>Count2: {count2}</h2>
+                <img src={avatar_url}></img>
+                <h1>Name: {name}</h1>
+                
+                {/* <h2>Count: {count}</h2>
+                <h2>Count2: {count2}</h2> */}
                 {/* Updating the state variable */}
-                <button onClick={() => {
+                {/* <button onClick={() => {
                     this.setState({
                       count : this.state.count+1,  
                       count2 : this.state.count2+2,
                     }
                     )
-                }}> Increase Count
-                </button>
+                }}> Increase Count */}
+                {/* </button> */}
             </div>
         );
    };
