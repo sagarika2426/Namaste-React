e9093c714d614fff403b1ab8dfb0a069285944e3
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-02-12 23:18:29 +0530

    tailwind config

---
+++ Code/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    // it means if you have one of these extension you can use tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
