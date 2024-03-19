c1a46810142af44a89329070250efca224a900c9
sagarika sahoo <sagarikasahoo16@gmail.com>
2024-03-19 23:43:35 +0530

    cart slice added

---
+++ Code/src/utils/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+
+    name: "cart",
+    initialState: {
+        items: [] //right now the cart is empty in the initial state
+
+    },
+    reducers: {
+        //Actions
+        addItem:(state, action) =>{//reducer fn
+            state.items.push(action.payload)
+        },
+        removeItem:(state) => {
+            state.items.pop() // remove the item from the last
+        },
+        clearCart: (state) => {
+            state.items.length = 0; //[] empty array
+        }
+        
+    }
+});
+
+export const {addItem, removeItem, clearCart} = cartSlice.actions;
+
+export default cartSlice.reducer
+
